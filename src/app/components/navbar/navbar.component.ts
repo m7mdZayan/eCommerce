1d164a9b2f4a7b6aba5804e5c7160895824be13c
EsraaFarhat <esraafarhat97@gmail.com>
2021-05-27 20:57:48 +0200

    Fixing navbar after refresh

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -11,17 +11,26 @@ import { Router } from '@angular/router';
 export class NavbarComponent implements OnInit {
   // @Input() userName = '';
   userExist: boolean = false;
-  userName = '';
-  id:string = '';
+  userName:any = '';
+  id:any = '';
   // id:string = "60a9bd6b6aa18d2e20b9e609";
   constructor(private http: HttpClient, private router: Router) {}
 
   ngOnInit(): void {
+    this.id = localStorage.getItem("user_id");
+    this.userName = localStorage.getItem("user_name");
+    
+    if(document.cookie){
+      this.userExist = true;
+    }
     Emitters.userEmitter.subscribe((user: any) => {
       this.userName = user.name;
       this.userExist = true;
       this.id = user._id;
-    });
+      localStorage.setItem("user_id",user._id);
+      localStorage.setItem("user_name",user.name);
+    },
+    (err) => console.log(err));
   }
 
   
@@ -35,5 +44,7 @@ export class NavbarComponent implements OnInit {
         this.userExist = false;
         // this.router.navigate(['login']);
       });
+      localStorage.removeItem("user_id");
+      localStorage.removeItem("user_name");
   }
 }
